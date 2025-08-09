8b9f7dfa2613f8b5e0c3bbce5892c9d541576a34
floklein <4128942+floklein@users.noreply.github.com>
2025-08-09 21:15:14 +0200

    loader

---
+++ apps/web/src/components/message.tsx
@@ -0,0 +1,28 @@
+import type { UIMessage } from "ai";
+import { Ellipsis } from "lucide-react";
+import { motion } from "motion/react";
+import { cn } from "@/lib/utils";
+
+export function Message({ message }: { message: UIMessage }) {
+  const parts = message.parts.filter((part) => part.type === "text");
+
+  return (
+    <motion.div
+      className={cn(
+        "max-h-[50svh] max-w-[80%] self-start overflow-y-auto whitespace-pre-wrap rounded-lg border-1 bg-secondary px-4 py-3",
+        message.role === "user" &&
+          "self-end bg-primary text-primary-foreground",
+      )}
+      key={message.id}
+      initial={{ opacity: 0, y: 10 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -10 }}
+      transition={{ duration: 0.2 }}
+    >
+      {parts.map((part) => (
+        <p key={part.type}>{part.text}</p>
+      ))}
+      {parts.length === 0 && <Ellipsis className="animate-pulse" />}
+    </motion.div>
+  );
+}
